660df0aa49579405698ad474ca032c12d14d2518
Jay Anne <baloloyjayanne@email.com>
2023-11-25 23:51:43 +0800

    2nd commit

--- src/app/Theme.service.ts
+++ src/app/Theme.service.ts
@@ -7,8 +7,15 @@ export class ThemeService {
   private darkMode = false;
 
   toggleMode(): void {
+    // this.darkMode = !this.darkMode;
+    // document.body.classList.toggle('dark-mode', this.darkMode);
+    // document.body.classList.toggle('light-mode', !this.darkMode);
+
     this.darkMode = !this.darkMode;
-    document.body.classList.toggle('dark-mode', this.darkMode);
-    document.body.classList.toggle('light-mode', !this.darkMode);
+  if (this.darkMode) {
+      document.body.classList.add('dark-mode');
+  } else {
+      document.body.classList.remove('dark-mode');
+  }
   }
 }
